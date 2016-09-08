57c1792b507bd8e015d593d4d0b5e271cd973a43
liDongDong <dllo@lidongdongdemac-mini.local>
2016-09-08 20:59:28 +0800

    完成主界面的基本搭建,5个fragment,配合tabLayout和viewPager使用,给tabLayout设置了selector,完成5个selector和5个tabLayout

--- GodCar/app/src/main/java/net/lidongdong/godcar/ui/activity/AbsActivity.java
+++ GodCar/app/src/main/java/net/lidongdong/godcar/ui/activity/AbsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 
 
 /**
@@ -16,7 +17,8 @@ public abstract class AbsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //本应用不显示顶端的通知栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //定制流程
         setContentView(setLayout());
         //初始化组件
--- GodCar/app/src/main/java/net/lidongdong/godcar/ui/activity/MainActivity.java
+++ GodCar/app/src/main/java/net/lidongdong/godcar/ui/activity/MainActivity.java
@@ -1,14 +1,26 @@
 package net.lidongdong.godcar.ui.activity;
 
 
-import android.view.View;
-import android.widget.TextView;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import net.lidongdong.godcar.R;
-import net.lidongdong.godcar.utils.T;
+import net.lidongdong.godcar.ui.fragment.ArticleFragment;
+import net.lidongdong.godcar.ui.fragment.FindCarFragment;
+import net.lidongdong.godcar.ui.fragment.FindFragment;
+import net.lidongdong.godcar.ui.fragment.ForumFragment;
+import net.lidongdong.godcar.ui.fragment.SettingFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AbsActivity{
-   private TextView tv;
+    private TabLayout mainTl;
+    private ViewPager mainVp;
+    private List<Fragment> fragments;
     @Override
     protected int setLayout() {
         return R.layout.activity_main;
@@ -16,16 +28,37 @@ public class MainActivity extends AbsActivity{
 
     @Override
     protected void initViews() {
-        tv=byView(R.id.tv);
+        mainTl=byView(R.id.main_tl);
+        mainVp=byView(R.id.main_vp);
     }
 
     @Override
     protected void initDatas() {
-        tv.setOnClickListener(new View.OnClickListener() {
+        fragments=new ArrayList<>();
+        //给集合添加fragment
+        fragments.add(new ArticleFragment());
+        fragments.add(new FindFragment());
+        fragments.add(new FindCarFragment());
+        fragments.add(new ForumFragment());
+        fragments.add(new SettingFragment());
+
+        mainVp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
-            public void onClick(View v) {
-                T.longMsg("自己的吐司");
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
             }
         });
+        mainTl.setupWithViewPager(mainVp);
+        mainTl.getTabAt(0).setCustomView(R.layout.tab_fragment_article);
+        mainTl.getTabAt(2).setCustomView(R.layout.tab_fragment_findcar);
+        mainTl.getTabAt(3).setCustomView(R.layout.tab_fragment_find);
+        mainTl.getTabAt(1).setCustomView(R.layout.tab_fragment_forum);
+        mainTl.getTabAt(4).setCustomView(R.layout.tab_fragment_setting);
+
     }
 }
