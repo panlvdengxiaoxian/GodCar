5562eab816f73a5b716cc68e3b76fe42e96498ef
liDongDong <dllo@lidongdongdemac-mini.local>
2016-09-08 20:01:27 +0800

    建立MainActivity的五个fragment和其的布局文件

---
+++ GodCar/app/src/main/java/net/lidongdong/godcar/ui/fragment/FindCarFragment.java
@@ -0,0 +1,23 @@
+package net.lidongdong.godcar.ui.fragment;
+
+import net.lidongdong.godcar.R;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class FindCarFragment extends AbsBaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_findcar;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
