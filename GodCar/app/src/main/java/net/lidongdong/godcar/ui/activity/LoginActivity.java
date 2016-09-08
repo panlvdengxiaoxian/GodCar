aca0e0320be57d9dbc47cbc30c1fc8a56f3dd990
liDongDong <dllo@lidongdongdemac-mini.local>
2016-09-08 19:40:33 +0800

    新建了登录界面Activity,欢迎界面的Activity

---
+++ GodCar/app/src/main/java/net/lidongdong/godcar/ui/activity/LoginActivity.java
@@ -0,0 +1,23 @@
+package net.lidongdong.godcar.ui.activity;
+
+import net.lidongdong.godcar.R;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class LoginActivity extends AbsActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
