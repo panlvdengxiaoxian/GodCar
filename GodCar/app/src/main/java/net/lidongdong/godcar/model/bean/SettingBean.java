dbd61c1ec42c8b741f1e7ee496f5048972950b87
liDongDong <dllo@lidongdongdemac-mini.local>
2016-09-10 22:01:20 +0800

    setting界面的设置

---
+++ GodCar/app/src/main/java/net/lidongdong/godcar/model/bean/SettingBean.java
@@ -0,0 +1,33 @@
+package net.lidongdong.godcar.model.bean;
+
+/**
+ * Created by dllo on 16/9/10.
+ */
+public class SettingBean  {
+    private String title;
+    private String  icon;
+
+    public SettingBean() {
+    }
+
+    public SettingBean(String title, String icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
